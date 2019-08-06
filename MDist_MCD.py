a2424d6621760932f88a6d4517797e879f7b040c
hngskj <hngskj@gmail.com>
2019-08-06 13:52:41 +0900

    Sample Code Calculate Mahalanobis distance using Minimum Covariance Determinant

---
+++ MDist_MCD.py
@@ -0,0 +1,67 @@
+"""
+Calculate Mahalanobis distance using Minimum Covariance Determinant
+ref) https://scikit-learn.org/stable/auto_examples/covariance/plot_mahalanobis_distances.html
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.covariance import EmpiricalCovariance, MinCovDet
+
+n_samples = 125
+n_outliers = 25
+n_features = 2
+
+# generate data
+gen_cov = np.eye(n_features)
+gen_cov[0, 0] = 2.
+X = np.dot(np.random.randn(n_samples, n_features), gen_cov)
+# add some outliers
+outliers_cov = np.eye(n_features)
+outliers_cov[np.arange(1, n_features), np.arange(1, n_features)] = 7.
+X[-n_outliers:] = np.dot(np.random.randn(n_outliers, n_features), outliers_cov)
+
+# fit a Minimum Covariance Determinant (MCD) robust estimator to data
+robust_cov = MinCovDet().fit(X)
+
+# compare estimators learnt from the full data set with true parameters
+emp_cov = EmpiricalCovariance().fit(X)
+
+# #############################################################################
+# Display results
+fig = plt.figure()
+plt.subplots_adjust(hspace=-.1, wspace=.4, top=.95, bottom=.05)
+
+# Show data set
+subfig1 = plt.subplot(3, 1, 1)
+inlier_plot = subfig1.scatter(X[:, 0], X[:, 1],
+                              color='black', label='inliers')
+outlier_plot = subfig1.scatter(X[:, 0][-n_outliers:], X[:, 1][-n_outliers:],
+                               color='red', label='outliers')
+subfig1.set_xlim(subfig1.get_xlim()[0], 11.)
+subfig1.set_title("Mahalanobis distances of a contaminated data set:")
+
+# Show contours of the distance functions
+xx, yy = np.meshgrid(np.linspace(plt.xlim()[0], plt.xlim()[1], 100),
+                     np.linspace(plt.ylim()[0], plt.ylim()[1], 100))
+zz = np.c_[xx.ravel(), yy.ravel()]
+
+mahal_emp_cov = emp_cov.mahalanobis(zz)
+mahal_emp_cov = mahal_emp_cov.reshape(xx.shape)
+emp_cov_contour = subfig1.contour(xx, yy, np.sqrt(mahal_emp_cov),
+                                  cmap=plt.cm.PuBu_r,
+                                  linestyles='dashed')
+
+mahal_robust_cov = robust_cov.mahalanobis(zz)
+mahal_robust_cov = mahal_robust_cov.reshape(xx.shape)
+robust_contour = subfig1.contour(xx, yy, np.sqrt(mahal_robust_cov),
+                                 cmap=plt.cm.YlOrBr_r, linestyles='dotted')
+
+subfig1.legend([emp_cov_contour.collections[1], robust_contour.collections[1],
+                inlier_plot, outlier_plot],
+               ['MLE dist', 'robust dist', 'inliers', 'outliers'],
+               loc="upper right", borderaxespad=0)
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
